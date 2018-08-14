e6cf81d7504a0501843ddea02ea8e1af9c4b275c
David Noh <davidnoh@mikes-MacBook-Pro-3.local>
2018-08-13 17:49:37 -0700

    aw

---
+++ apps/regi/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+from django.contrib import messages
+from .models import User
+import bcrypt
+
+def index(request):
+    return render(request, ("regi/index.html"))
+
+def success(request):
+    if "user_id" not in request.session:
+        return redirect('/')
+    else:
+        id=request.session["user_id"]
+        return render(request, ("regi/success.html"), {"stuff":User.objects.get(id=id)})
+
+def register(request):
+    if request.method =="POST":
+        errors = User.objects.validator(request.POST)
+        request.session['first_name'] = request.POST['first_name']
+        request.session['last_name'] = request.POST['last_name']
+        request.session['email'] = request.POST['email']
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value, extra_tags=key)
+            return redirect('/')
+        else:
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            pwhashed = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+
+            User.objects.create(first_name=first_name, last_name=last_name, email=email, password=pwhashed) 
+            messages.success(request, "Successfully added")
+            request.session.clear()
+            return redirect('/')
+    return redirect("/")
+
+def login(request):
+    if request.method =="POST":
+        email = request.POST['email']
+        user = User.objects.get(email = email)
+        if user:
+            if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+                id= user.id
+                messages.success(request, "Login Success")
+                request.session['user_id'] = id
+                return redirect('/success')
+            else:
+                messages.error(request, "Login Fail")
+                return redirect("/")
+        else:
+            messages.error(request, "Login Fail")
+            return redirect("/")
+    return redirect("/")
+
+def clear(request):
+    request.session.clear()
+    print("Session Cleared")
+    messages.success(request, "Session Cleared")
+    return redirect ("/")
